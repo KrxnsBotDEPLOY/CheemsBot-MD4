d2de63f6e87ea161b355c0d89c25331a91b45a66
KrxnsBotDEPLOY <kronoshacking07@gmail.com>
2024-11-24 07:36:08 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,11 +54,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['50938578360'] //ur owner number
+global.ownername = "🐲𒁂🦠 ⚜️ KRONOS!🦇" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "India, Mizoram, Haiti" //ur location
 
 //bot bomdy 
 global.owner = ['916909137213']
